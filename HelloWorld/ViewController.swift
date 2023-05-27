bc0fa462f52c439e368a42318d6dec89f6f5980a
yuniyolli <yuniyolli@gmail.com>
2023-05-27 09:56:02 +0900

    feat: botton to show Hello + "name"

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblHello: UILabel!
+    
+    @IBOutlet var txtName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
 }
 
